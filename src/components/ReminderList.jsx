8ee34125bcdcd307a814ff4a65f97465922c5164
bewpage <b.jedlinski@bewpage.com>
2017-10-03 16:49:47 +0200

    date order function

--- src/components/ReminderList.jsx
+++ src/components/ReminderList.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import { connect } from 'react-redux';
 import { reminderRef } from "../firebase";
 import { setReminders } from "../actions/index";
 import ReminderItem from "./ReminderItem";
+import moment from 'moment';
 
 class ReminderList extends Component{
 
@@ -15,17 +17,28 @@ class ReminderList extends Component{
             });
             this.props.setReminders(reminders);
         })
-}
+    }
+
+    sortByDate(e){
+        // const reminders = this.props.reminders;
+        // console.log('old reminders', reminders);
+        const newReminders = [].concat(e).sort((a, b) => moment.utc(a.dueDate).diff(moment.utc(b.dueDate)));
+        console.log('sorted newReminders', newReminders);
+        return newReminders;
+    };
 
     render(){
         // console.log('this.props.reminders', this.props.reminders);
+        const remindersArray = this.props.reminders
+        const remindersInDateOrder = this.sortByDate(remindersArray);
+        console.log('remindersInDateOrder', remindersInDateOrder);
         return(
             <div style={{margin: '5px'}}>
                 <h4>Reminder List</h4>
                 <div className='row'>
                     <ul className='list-group col-sm-4'>
                         {
-                            this.props.reminders.map((reminder, index) => {
+                            remindersInDateOrder.map((reminder, index) => {
                                 return (
                                         <ReminderItem key={index} reminder={reminder} />
                                 )
